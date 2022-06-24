f04eb25a55a9f13eeecbf4ce5d175a9a96de8cc6
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-06-24 16:12:06 +0300

    Sqrt numbers

--- Task_00/Zad0.cs
+++ Task_00/Zad0.cs
@@ -1,1 +1,5 @@
-﻿ 
+﻿ // Вывести квадрат числа 4, 3, 7
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
+Console.Write($"Квадрат числа {number} = {sqrt}");
