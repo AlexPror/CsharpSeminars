5cf396ab17afb64099c54ae6b8ff8e296f86ebcb
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-06-24 16:10:43 +0300

    numbers from -N to N

---
+++ Task_05/Program.cs
@@ -0,0 +1,17 @@
+﻿// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые
+//числа в промежутке от -N до N.
+Console.Write("Введите целое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = -numberN;
+while (numberN >= count) 
+{
+    Console.Write($"{count} ");
+    count +=1;
+}
+
+// Console.Write("Введите целое число: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+// for (int i = -numberN; i <= numberN; i++)
+// {
+//     Console.Write($"{i} ");
+// }
