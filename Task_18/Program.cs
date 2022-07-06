12523a77b56480ec4af8aa3359bfec906ed128d2
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2022-07-06 16:15:25 +0300

    Cordinate

---
+++ Task_18/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 18: Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y).
+*/
+
+
+Console.WriteLine("Введите номер плоскости: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string Coord(int quarter)
+{
+    switch (quarter)
+    {
+        case 1: return "X > 0; Y > 0";
+        case 2: return "X < 0; Y > 0";
+        case 3: return "X < 0; Y < 0";
+        case 4: return "X > 0; Y < 0";
+        default: return "введено некорректное число";
+    }
+}
+string result = Coord(quarter);
+Console.WriteLine(result);
